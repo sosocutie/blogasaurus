d023109acc418acfa03e73d15945a95caf7535e5
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-18 10:34:57 -0400

    Added post section

--- main.py
+++ main.py
@@ -12,6 +12,25 @@ class blogasaurus(webapp2.RequestHandler):
         start_template=jinja_env.get_template("templates/index.html")
         self.response.write(start_template.render())
 
+class BlogHandler(webapp2.RequestHandler):
+    def get(self):
+        start_template=jinja_env.get_template("templates/new_post.html")
+        self.response.write(start_template.render())
+    def post(self):
+        title_var = self.request.get('post_title')
+        post_var = self.request.get('post_content')
+        author_var = self.request.get('author_name')
+
+        post_vars = {
+            "title_var": title_var,
+            "post_var": post_var,
+            "author_var": author_var,
+            "date_and_time": "food",
+            }
+        template = jinja_env.get_template('templates/view_post.html')
+        self.response.write(template.render(post_vars))
+
 app = webapp2.WSGIApplication([
-    ('/', blogasaurus)
+    ('/', blogasaurus),
+    ('/make_post', BlogHandler)
 ], debug=True)
